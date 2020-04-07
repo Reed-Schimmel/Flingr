82653ac9333246d9e24663c0dfd9f9f39045b99e
Reed Schimmel <reeder960@gmail.com>
2020-04-07 17:36:34 -0500

    starting works

--- App.js
+++ App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import {
   Text,
-  View,
   StyleSheet,
+  SafeAreaView,
+  StatusBar
 } from 'react-native';
 
 const Flingr = (props) => {
-    return (
-      <View style={styles.container}>
+      return (
+      <SafeAreaView style={styles.container}>
         <Text>Hello World</Text>
-        <Text>Hello World</Text>
-        <Text>Hello World</Text>
-        <Text>Hello World</Text>
-        <Text>Hello World</Text>
-        <Text>Hello World</Text>
-      </View>
+      </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: StatusBar.currentHeight, // for Android. If this looks weird in iOS tell Reed.
     backgroundColor: '#FFF'
   },
 });
