6068a18cccd86f68fa70c8350496e6ee930146d9
Kam Bielawski <kamtb28@gmail.com>
2020-04-23 12:38:17 -0500

    authentication modal

---
+++ src/components/AuthenticationModal.js
@@ -0,0 +1,25 @@
+import React, { useState, useContext } from 'react';
+import { View } from 'react-native';
+
+import { Context as GlobalContext } from '../context/GlobalContext';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+
+const AuthenticationModal = () => {
+  const { state: { userAuth } } = useContext(GlobalContext);
+
+  const [showLogin, setShowLogin] = useState(true);
+
+  if (userAuth) return null;
+  
+  return (
+    <>
+      {showLogin
+        ? <LoginScreen navigateRegister={() => setShowLogin(false)} />
+        : <RegisterScreen navigateLogin={() => setShowLogin(true)} />
+      }
+    </>
+  );
+};
+
+export default AuthenticationModal;
