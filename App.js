b029c51254c8459ed08f403cfc48503de6e5e42b
Kam Bielawski <kamtb28@gmail.com>
2020-04-23 12:38:07 -0500

    initialize firebase

--- App.js
+++ App.js
@@ -7,10 +7,16 @@ import {
   Dimensions,
   //Animate
 } from 'react-native';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import { Provider as GlobalProvider } from './src/context/GlobalContext';
 import RegisterScreen from './src/screens/RegisterScreen';
+import firebaseConfig from './firebaseConfig.json';
+
+firebase.initializeApp(firebaseConfig);
 
 const Flingr = (props) => {
 
