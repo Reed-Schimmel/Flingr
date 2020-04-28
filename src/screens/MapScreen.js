98a6cbdaf7e1c1d7f366569256b62e8fd5b3a006
Amir Shami <amirshami@ku.edu>
2020-04-28 13:10:35 -0500

    old map

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,57 +1,40 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native'; //Animate
+import { StyleSheet, Dimensions } from 'react-native'; //Animate
 import MapView, { Marker } from 'react-native-maps';
 import { Context } from '../context/GlobalContext';
-import FloatingButton from '../components/FloatingButton';
 
 const screen = Dimensions.get('window');
 const ASPECT_RATIO = screen.width / screen.height;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-const getCurrentLocation = () => {
-  return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
-  });
-};
-
 const MapScreen = (props) => { // TODO: very slow buttons bug
   const { /*queryNewBaseLocations,*/ state } = useContext(Context);
   const { /*renderedBases, queryBasesError,*/ coords } = state;
 
-  const [states, setState] = useState({
-    region: {
-        latitude: 39,
-        longitude: -98,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-    },
-});
+  const [region, setRegion] = useState({
+    latitude: 39,
+    longitude: -98,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  });
 
   useEffect(() => {
-    getCurrentLocation().then(position => {
-        if (position) {
-            setState({
-                region: {
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                    latitudeDelta: LATITUDE_DELTA,
-                    longitudeDelta: LONGITUDE_DELTA
-                },
-            });
-        }
-    })
-}, []);
-
+    console.log(coords);
+    // update region state every time the global context has new coords
+    setRegion({
+      latitude: coords.latitude || region.latitude,
+      longitude: coords.longitude || region.longitude,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA
+    });
+  }, [coords.latitude === null]);
+console.log(state.userData)
   //const { userAuth, loginError } = useContext(Context)
   return (
-    <MapView style={[styles.map, props.style]} region={states.region} showsUserLocation={props.userBaseLocation === false ? true : false}>
-      {props.userBaseLocation === true ? <Marker coordinate={states.region}> 
-        <FloatingButton title = {state.userData.username} style = {styles.marker}/> 
-        <View style = {styles.stick}></View>
-      </Marker> : null}
+    <MapView style={[styles.map, props.style]} region={region} showsUserLocation={false}>
+      <Marker title = {state.userData.username} coordinate={region} />
     </MapView>
-
   );
 };
 
@@ -63,29 +46,7 @@ const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-  marker: {
-    position: 'relative',
-    right: 0,
-    bottom: 0,
-    height: 50,
-    width: 100,
-    top: -25,
-    borderRadius: 50,
-    zIndex: 0,
-    backgroundColor: 'lightblue',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    shadowColor: '#000',
-  },
-  stick: {
-    position: 'relative',
-    top: -35,
-    left: 45,
-    backgroundColor: 'lightblue',
-    width: 5,
-    height: 50,
-  },
 });
 
 export default MapScreen;
+
