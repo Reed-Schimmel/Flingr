e451a60f9485aca89707ed377a6160276042a724
Amir Shami <amirshami@ku.edu>
2020-04-28 13:46:11 -0500

    comments

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -9,38 +9,38 @@ const ASPECT_RATIO = screen.width / screen.height;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-const getCurrentLocation = () => {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
-  });
-};
+// const getCurrentLocation = () => {
+//   return new Promise((resolve, reject) => {
+//     navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
+//   });
+// };
 
 const MapScreen = (props) => {
-  const {state} = useContext(Context);
+//   const {state} = useContext(Context);
 
-  const [states, setState] = useState({
-    region: {
-      latitude: 39,
-      longitude: -98,
-      latitudeDelta: LATITUDE_DELTA,
-      longitudeDelta: LONGITUDE_DELTA,
-    },
-  });
+//   const [states, setState] = useState({
+//     region: {
+//       latitude: 39,
+//       longitude: -98,
+//       latitudeDelta: LATITUDE_DELTA,
+//       longitudeDelta: LONGITUDE_DELTA,
+//     },
+//   });
 
-  useEffect(() => {
-    getCurrentLocation().then(position => {
-      if (position) {
-        setState({
-          region: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA
-          },
-        });
-      }
-    });
-  }, []);
+//   useEffect(() => {
+//     getCurrentLocation().then(position => {
+//       if (position) {
+//         setState({
+//           region: {
+//             latitude: position.coords.latitude,
+//             longitude: position.coords.longitude,
+//             latitudeDelta: LATITUDE_DELTA,
+//             longitudeDelta: LONGITUDE_DELTA
+//           },
+//         });
+//       }
+//     });
+//   }, []);
 
   return (
     <MapView style={[styles.map, props.style]} region={states.region} showsUserLocation={props.userBaseLocation === false ? true : false}>
