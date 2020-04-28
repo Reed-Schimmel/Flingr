df9be2dc6c074a4b3714f9aeee60362132146729
Amir Shami <amirshami@ku.edu>
2020-04-28 13:21:22 -0500

    indentation for useeffect

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -8,38 +8,44 @@ const ASPECT_RATIO = screen.width / screen.height;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-// const getCurrentLocation = () => {
-//   return new Promise((resolve, reject) => {
-//     navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
-//   });
-// };
+const getCurrentLocation = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
+  });
+};
 
 const MapScreen = (props) => { // TODO: very slow buttons bug
   const { /*queryNewBaseLocations,*/ state } = useContext(Context);
   const { /*renderedBases, queryBasesError,*/ coords } = state;
 
-  const [region, setRegion] = useState({
-    latitude: 39,
-    longitude: -98,
-    latitudeDelta: LATITUDE_DELTA,
-    longitudeDelta: LONGITUDE_DELTA,
-  });
+  const [states, setState] = useState({
+    region: {
+      latitude: 39,
+      longitude: -98,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    },
+});
 
   useEffect(() => {
-    console.log(coords);
-    // update region state every time the global context has new coords
-    setRegion({
-      latitude: coords.latitude || region.latitude,
-      longitude: coords.longitude || region.longitude,
-      latitudeDelta: LATITUDE_DELTA,
-      longitudeDelta: LONGITUDE_DELTA
-    });
-  }, [coords.latitude === null]);
+    getCurrentLocation().then(position => {
+      if (position) {
+        setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA
+          },
+        });
+      }
+    })
+}, []);
 
   //const { userAuth, loginError } = useContext(Context)
   return (
-    <MapView style={[styles.map, props.style]} region={region} showsUserLocation={true}>
-      <Marker coordinate={region} />
+    <MapView style={[styles.map, props.style]} region={states.region} showsUserLocation={true}>
+      <Marker coordinate={states.region} />
     </MapView>
   );
 };
