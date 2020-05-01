603512a623d176e0d6270085a538e813d5babf37
Reed Schimmel <reeder960@gmail.com>
2020-05-01 12:56:33 -0500

    fire screen crashing app

--- src/ar/FireProjectileScene.js
+++ src/ar/FireProjectileScene.js
@@ -1,4 +1,7 @@
-'use strict';
+/**
+ * @author Reed Schimmel
+ * @description Shoot hoops across da world.
+ */
 import React, { Component } from 'react';
 import { Alert } from 'react-native';
 import {
@@ -8,7 +11,11 @@ import {
   ViroLightingEnvironment,
   ViroConstants,
   ViroMaterials,
+  ViroCamera,
 } from 'react-viro';
+
+const ballPhysics = { friction: 0.6, type: 'Dynamic', mass: 4, enabled: true, useGravity: false, shape: { type: 'Sphere', params: [0.14] }, restitution: 0.65 };
+
 
 export default class FireProjectileScene extends Component {
   constructor(props) {
@@ -23,45 +30,42 @@ export default class FireProjectileScene extends Component {
     this.sceneRef = undefined;
     this.ball = undefined;
 
+
     this._onInitialized = this._onInitialized.bind(this);
+    this.launchBall = this.launchBall.bind(this);
   }
   // TODO: use this.props.saveLaunch({ coords }) to send shizz to firebase
-
-  componentDidMount() {
-    // AsyncStorage.getItem('base').then(base => this.setState({ base: JSON.parse(base) }))
-    // AsyncStorage.getItem('base').then(base => this.plane.setNativeProps(JSON.parse(base)) )
-    // AsyncStorage.getItem('base').then(base => this.setState({ base: JSON.parse(base) }));
-  }
 
   render() {
     return (
       <ViroARScene
         onTrackingUpdated={this._onInitialized}
-        // anchorDetectionTypes='PlanesHorizontal'
         physicsWorld={{ gravity: [0, -9.81, 0] }} ref={(component) => { this.sceneRef = component; }}
-      // rotation={this.state.heading} https://github.com/viromedia/viro/issues/118#issuecomment-352500764
       >
         <ViroAmbientLight color={'#FFFFFF'} intensity={10} castsShadow={true} />
         <ViroLightingEnvironment source={require('./res/ibl_envr.hdr')} />
-
-
-        {/* A Single Ball we have spawned in our scene */}
-        <Viro3DObject ref={(obj) => { this.ball = obj; }}
-          source={require('./res/object_basketball_pbr.vrx')}
-          scale={[0.5, 0.5, 0.5]}
-          position={[0, 0, 1.0]}
-          rotation={[0, 0, 0]}
-          resources={[
-            require('./res/blinn1_Base_Color.png'),
-            require('./res/blinn1_Metallic.png'),
-            require('./res/blinn1_Roughness.png'),
-            require('./res/blinn1_Normal_OpenGL.png')]}
-          type="VRX"
-          physicsBody={{ friction: 0.6, type: 'Dynamic', mass: 4, enabled: true, useGravity: true, shape: { type: 'Sphere', params: [0.14] }, restitution: 0.65 }}
-          viroTag="BallTag"
-        // onClick={this.state.controllerConfig == CONTROLLER_PUSH ? this.onItemPushImpulse("BallTag") : undefined}
-        // onDrag={this.state.controllerConfig == CONTROLLER_GRIP ? () => { } : undefined}
-        />
+        <ViroCamera position={[0, 0, 0]} active={true} >
+          <Viro3DObject ref={(obj) => { this.ball = obj; }}
+            source={require('./res/object_basketball_pbr.vrx')}
+            scale={[0.5, 0.5, 0.5]}
+            position={[0, 0, -1]}
+            rotation={[0, 0, 0]}
+            resources={[
+              require('./res/blinn1_Base_Color.png'),
+              require('./res/blinn1_Metallic.png'),
+              require('./res/blinn1_Roughness.png'),
+              require('./res/blinn1_Normal_OpenGL.png')]}
+            type="VRX"
+            physicsBody={ballPhysics}
+            onLoadEnd={() => this.ball.setNativeProps({ 'position': [0, 0, -1] })}
+            viroTag="BallTag"
+          // highAccuracyEvents={true}
+          // onClick={this.state.controllerConfig == CONTROLLER_PUSH ? this.onItemPushImpulse("BallTag") : undefined}
+          // onClick={() => this.ball.applyImpulse([0,0,-1],[0,0,-1])}
+          // onClick={this.launchBall}
+          // onDrag={this.state.controllerConfig == CONTROLLER_GRIP ? () => { } : undefined}
+          />
+        </ViroCamera>
       </ViroARScene>
     );
   }
@@ -72,6 +76,16 @@ export default class FireProjectileScene extends Component {
     } else if (state == ViroConstants.TRACKING_NONE) {
       Alert.alert('Lost tracking!');
     }
+  }
+
+  launchBall() { // TODO: fix this shizz
+    this.sceneRef.getCameraOrientationAsync().then(({ position }) => {
+      const force = 5; // newtons
+      const impulse = position.map(axis => axis * force);
+      this.ball.setNativeProps({ 'physicsBody': { ...ballPhysics, useGravity: true } }); // turn on gravity
+      // Now smack it
+      this.ball.applyImpulse(impulse, position); // dis code not werkin'
+    });
   }
 }
 
